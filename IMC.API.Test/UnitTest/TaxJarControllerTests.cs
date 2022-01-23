5ca88c94e7b6e0939b778402e5a9e0b4129b180f
Francis Elendu <elcfran6@hotmail.com>
2022-01-22 19:45:02 -0600

    Added unit test for NotFound testing

---
+++ IMC.API.Test/UnitTest/TaxJarControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using IMC.API.Controllers;
+using IMC.API.Models;
+using IMC.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IMC.API.Test.UnitTest
+{
+    public class TaxJarControllerTests
+    {
+        [Fact]
+        public async Task GetRateByLocation_WithUnexistingLocation_ReturnsNotFound()
+        {
+            //Arrange
+            var serviceStub = new Mock<ITaxJarService>();
+            serviceStub.Setup(service => service.GetTaxRates(It.IsAny<string>()))
+                .ReturnsAsync((TaxRate)null);
+
+            var controller = new TaxJarController(serviceStub.Object);
+
+            //Act
+
+            var result = await controller.GetRateByLocation(string.Empty);
+
+            //Assert
+
+            result.ToString().Equals(HttpStatusCode.NotFound);
+        }
+
+    }
+}
